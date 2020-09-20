7258c8e9244e7222c3c48cc3faf2fef9743212a8
J0R3D1N <joredin@prkrfamily.com>
2020-09-20 14:44:25 -0700

    removed legacy content

--- Scripts/Network_Monitor/NetMonCleanUp.ps1
+++
@@ -1,5 +0,0 @@
-﻿# Netmon clean up script
-$path = "C:\Windows\Utilities\NetMon"
-If (Test-Path -Path $path) {
-    Get-ChildItem -Path $path -Filter *.cap | ? {$_.LastWriteTime -lt (Get-Date).AddDays(-3)} | Remove-Item -Force
-}
