e03e76ea0cb2c641e897a8016c2cd6536af1d5c8
msft-jasonparker <jason.parker@microsoft.com>
2020-11-07 19:33:56 -0700

    added better logic for dsc processing has integrated rds agent install

--- Deployment/DscConfigurations/WVD-Win10-SessionHost-Config.ps1
+++ Deployment/DscConfigurations/WVD-Win10-SessionHost-Config.ps1
@@ -11,26 +11,22 @@ Configuration WvdSessionHostConfig
     Set-ExecutionPolicy "RemoteSigned" -Scope Process -Confirm:$false
     Set-ExecutionPolicy "RemoteSigned" -Scope CurrentUser -Confirm:$false
 
+    # DSC Modules
     Import-DSCResource -ModuleName "PSDesiredStateConfiguration"
-    Import-DSCResource -ModuleName "xPSDesiredStateConfiguration"
-    Import-DscResource -ModuleName "CertificateDsc"
-    Import-DscResource -ModuleName "ComputerManagementDsc"
-    Import-DscResource -ModuleName "PowerShellModule"
+    Import-DSCResource -ModuleName "xPSDesiredStateConfiguration" -ModuleVersion 9.1.0
+    Import-DscResource -ModuleName "CertificateDsc" -ModuleVersion 5.0.0
+    Import-DscResource -ModuleName "ComputerManagementDsc" -ModuleVersion 8.4.0
     Import-Module -Name BitsTransfer -force
 
-    $wvdConfigDataPath = "C:\ProgramData\WVDConfiguration"
+    $wvdConfigDataPath = "C:\ProgramData\WVD-Automated-Deployment"
     Start-BitsTransfer -Source $wvdDscConfigZipUrl -Destination ($env:TEMP + "\wvdConfiguration.zip")
     If (Test-Path -Path ($env:TEMP + "\wvdConfiguration.zip")) { Expand-Archive -Path ($env:TEMP + "\wvdConfiguration.zip") -DestinationPath ($wvdConfigDataPath + "\WVD") -Force}
-    
-    Start-BitsTransfer -Source "\\servername\sharename\wvdartifacts\wvd_packages.zip" -Destination ($env:TEMP + "\wvd_packages.zip")
-    If (Test-Path -Path ($env:TEMP + "\wvd_packages.zip")) { Expand-Archive -Path ($env:TEMP + "\wvd_packages.zip") -DestinationPath ($wvdConfigDataPath + "\DSC") -Force}
     
     . ($wvdConfigDataPath + "\WVD\Functions.ps1")
 
     Node localhost {
         # LCM Settings
         LocalConfigurationManager {
-            ConfigurationMode = "ApplyOnly"
             RebootNodeIfNeeded = $true
         }
         ## Local Administrators
@@ -43,17 +39,102 @@ Configuration WvdSessionHostConfig
                 Else { Return $false }
             }
         }
-        # Create configuration data directory
-        File CreateConfigDataPath {
-            Ensure = "Present"
-            Type = "Directory"
-            DestinationPath = $wvdConfigDataPath
+        ## Domain Join Sleep
+        Script DomainJoinSleep {
+            TestScript = { return (Test-Path ("{0}\DomainJoinSleep.txt" -f $wvdConfigDataPath)) }
+            SetScript = {
+                New-Item -Path $using:wvdConfigDataPath -Name "DomainJoinSleep.txt" -ItemType "file"
+                Start-Sleep -Seconds 60
+            }
+            GetScript = {
+                @{ Result = (Test-Path (Test-Path ("{0}\DomainJoinSleep.txt" -f $wvdConfigDataPath)) }
+            }
         }
-        #Create directory for package zip file
+        ## Local Administrators
+        Script LocalAdministratorsGroup {
+            GetScript = { return @{'Result' = ''} }
+            SetScript = { Add-LocalGroupMember -Group "Administrators" -Member "PRKRLABS\WVDAdmins" -ErrorAction SilentlyContinue }
+            TestScript = {
+                $Members = Get-LocalGroupMember -Group "Administrators" | Foreach-Object {$_.Name}
+                If ($Members -contains "PRKRLABS\WVDAdmins") { Return $true }
+                Else { Return $false }
+            }
+        }
+        # WVD Packages
+        Script WVDSoftwarePackage {
+            GetScript = { Return @{ 'Result' = '' } }
+            TestScript = {
+                If (Test-Path -Path ($env:TEMP + "\wvd_packages.zip")) {
+                    $currentZip = Get-ChildItem -Path ($env:TEMP + "\wvd_packages.zip")
+                    $remoteZip = Get-ChildItem -Path ($using:wvdArtifactLocation + "\wvd_packages.zip")
+                    return ($currentZip.CreationTimeUtc -ge $remoteZip.CreationTimeUtc)
+                }
+                else { return $false }
+            }
+            SetScript = {
+                Start-BitsTransfer -Source ($using:wvdArtifactLocation + "\wvd_packages.zip") -Destination ($env:TEMP + "\wvd_packages.zip")
+                If (Test-Path -Path ($env:TEMP + "\wvd_packages.zip")) { Expand-Archive -Path ($env:TEMP + "\wvd_packages.zip") -DestinationPath ($using:vaConfigDataPath + "\DSC") -Force }
+            }
+        }
+        # Powershell Modules
+        Script PowerShellModules {
+            GetScript = { Return @{ 'Result' = '' } }
+            TestScript = { Return $false }
+            SetScript = {
+                Find-PackageProvider NuGet -Force | Install-PackageProvider -Force | Out-Null
+                Install-PackageProvider PowerShellGet -Force | Out-Null
+
+                # Check / Install Azure PowerShell Module
+                Write-Verbose "Checking Az Module"
+                $azModuleCheck = Get-InstalledModule Az -ErrorAction SilentlyContinue
+                If ($azModuleCheck) {
+                    $azModule = Find-Module Az
+                    If ($azModule.Version -gt $azModuleCheck.Version) { $azModule | Install-Module -Force -AllowClobber }
+                }
+                Else { Find-Module Az | Install-Module -Force }
+
+                # Check / Install Azure AD PowerShell Module
+                Write-Verbose "Checking AzureAD Module"
+                $azureADModuleCheck = Get-InstalledModule AzureAD -ErrorAction SilentlyContinue
+                If ($azureADModuleCheck) {
+                    $azureADModule = Find-Module AzureAD
+                    If ($azureADModule.Version -gt $azureADModuleCheck.Version) { $azureADModule | Install-Module -Force -AllowClobber }
+                }
+                Else { Install-Module -Name AzureAD -Force }
+
+                # Check / Install SharePoint Online PowerShell Module
+                Write-Verbose "Checking Microsoft.Online.SharePoint.PowerShell Module"
+                $spoModuleCheck = Get-InstalledModule "Microsoft.Online.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+                If ($spoModuleCheck) {
+                    $spoModule = Find-Module "Microsoft.Online.SharePoint.PowerShell"
+                    If ($spoModule.Version -gt $spoModuleCheck.Version) { $spoModule | Install-Module -Force -AllowClobber }
+                }
+                Else { Install-Module -Name "Microsoft.Online.SharePoint.PowerShell" -Force }
+
+                # Check / Install Exchange Online PowerShell Module
+                Write-Verbose "Checking ExchangeOnlineManagement Module"
+                $exoModuleCheck = Get-InstalledModule ExchangeOnlineManagement -ErrorAction SilentlyContinue
+                If ($exoModuleCheck) {
+                    $exoModule = Find-Module ExchangeOnlineManagement
+                    If ($exoModule.Version -gt $exoModuleCheck.Version) { $exoModule | Install-Module -Force -AllowClobber }
+                }
+                Else { Install-Module -Name ExchangeOnlineManagement -Force }
+
+                # Check / Install SQL Server PowerShell Module
+                Write-Verbose "Checking SqlServer Module"
+                $sqlModuleCheck = Get-InstalledModule SqlServer -ErrorAction SilentlyContinue
+                If ($sqlModuleCheck) {
+                    $sqlModule = Find-Module SqlServer
+                    If ($sqlModule.Version -gt $sqlModuleCheck.Version) { $sqlModule | Install-Module -Force -AllowClobber }
+                }
+                Else { Install-Module -Name SqlServer -Force }
+            }
+        }
+        # Ensure directory for packages exists
         File CreatePackageDirectory {
             Ensure = "Present"
             Type = "Directory"
-            DestinationPath = ("{0}\DSC" -f $wvdConfigDataPath)
+            DestinationPath = ("{0}\DSC\Packages" -f $wvdConfigDataPath)
         }
         #Create directory for MSI logs
         File CreateLogDirectory {
@@ -71,11 +152,17 @@ Configuration WvdSessionHostConfig
                     Ensure = "Present"
                     Name = "RDS-RD-Server"
                 }
+
+                PendingReboot AfterRDSFeatureInstall {
+                    Name = "AfterRDSFeatureInstall"
+                    DependsOn = "[WindowsFeature]RDS-RD-Server"
+                }
     
                 Script ExecuteRdAgentInstallServer {
-                    DependsOn = "[WindowsFeature]RDS-RD-Server","[xArchive]ExtractPackages"
+                    DependsOn = "[WindowsFeature]RDS-RD-Server","[File]CreatePackageDirectory"
                     GetScript = { return @{'Result' = ''} }
                     SetScript = {
+                        . ($using:wvdConfigDataPath + "\WVD\Functions.ps1")
                         try { & "$using:wvdConfigDataPath\WVD\Script-AddRdshServer.ps1" -HostPoolName $using:HostPoolName -RegistrationInfoToken $using:RegistrationInfoToken -EnableVerboseMsiLogging:($using:EnableVerboseMsiLogging) }
                         catch {
                             $ErrMsg = $PSItem | Format-List -Force | Out-String
@@ -84,6 +171,7 @@ Configuration WvdSessionHostConfig
                         }
                     }
                     TestScript = {
+                        . ($using:wvdConfigDataPath + "\WVD\Functions.ps1")
                         try { return (Test-path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent") }
                         catch {
                             $ErrMsg = $PSItem | Format-List -Force | Out-String
@@ -91,14 +179,15 @@ Configuration WvdSessionHostConfig
                             throw [System.Exception]::new("Some error occurred in DSC ExecuteRdAgentInstallServer TestScript: $ErrMsg", $PSItem.Exception)
                         }
                     }
-                }
+                }  
             }
             else {
                 ("[{0}] {1} | rdshIsServer = FALSE" -f (Get-Date),$env:COMPUTERNAME) | Out-File "$wvdConfigDataPath\WVD\rdshIsServer_Results.log" -Append
                 Script ExecuteRdAgentInstallClient {
-                    DependsOn = "[xArchive]ExtractPackages"
+                    DependsOn = "[File]CreatePackageDirectory"
                     GetScript = { return @{'Result' = ''} }
                     SetScript = {
+                        . ($using:wvdConfigDataPath + "\WVD\Functions.ps1")
                         try { & "$using:wvdConfigDataPath\WVD\Script-AddRdshServer.ps1" -HostPoolName $using:HostPoolName -RegistrationInfoToken $using:RegistrationInfoToken -EnableVerboseMsiLogging:($using:EnableVerboseMsiLogging) }
                         catch {
                             $ErrMsg = $PSItem | Format-List -Force | Out-String
@@ -107,6 +196,7 @@ Configuration WvdSessionHostConfig
                         }
                     }
                     TestScript = {
+                        . ($using:wvdConfigDataPath + "\WVD\Functions.ps1")
                         try { return (Test-path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent") }
                         catch {
                             $ErrMsg = $PSItem | Format-List -Force | Out-String
@@ -115,10 +205,25 @@ Configuration WvdSessionHostConfig
                         }
                     }
                 }
+                #VDI/WVD Optimization Script
+                Script VDI_Optimize {
+                    TestScript = {
+                        if ((Get-ScheduledTask MapsToastTask).State -eq "Disabled") { return $true }
+                        else { return $false }
+                    }
+                    SetScript = { 
+                        $version = (Get-ItemProperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\").ReleaseId
+                        If($version -eq "20H2" -or $version -eq "2009") { $version = "2004" }
+                        powershell.exe -NonInteractive -File "$using:wvdConfigDataPath\DSC\Packages\optimizations\Win10_VirtualDesktop_Optimize.ps1" -WindowsVersion $version                    }
+                    GetScript = {
+                        @{ Result = ((Get-ScheduledTask MapsToastTask).State) }
+                    }
+                    DependsOn = "[File]CreatePackageDirectory"
+                }
             }
         }
-        Else { ("[{0}] {1} | Failed to find isRdshServer command - module didn't import" -f (Get-Date),$env:COMPUTERNAME) | Out-File "$wvdConfigDataPath\WVD\rdsh_function_import.log" -Append }
-
+        Else { ("[{0}] {1} | Failed to find isRdshServer command - module didn't import" -f (Get-Date),$env:COMPUTERNAME) | Out-File "$vaConfigDataPath\WVD\rdsh_function_import.log" -Append }
+        
         ## Network Tracing
         File CreateUtilitiesDirectory {
             Ensure = "Present"
@@ -177,70 +282,6 @@ Configuration WvdSessionHostConfig
             MultipleInstances = "IgnoreNew"
             RunOnlyIfIdle = $false
             DependsOn = "[ScheduledTask]EnableNetMonTracing"
-        }
-        If ($DeploymentFunction -eq "ADM") {
-            WindowsCapability AdminToolsFileServices {
-                Ensure = "Present"
-                Name = "Rsat.FileServices.Tools~~~~0.0.1.0"
-            }
-            WindowsCapability AdminToolsDHCP {
-                Ensure = "Present"
-                Name = "Rsat.DHCP.Tools~~~~0.0.1.0"
-            }
-            WindowsCapability AdminToolsDNS {
-                Ensure = "Present"
-                Name = "Rsat.Dns.Tools~~~~0.0.1.0"
-            }
-            WindowsCapability AdminToolsActiveDirectory {
-                Ensure = "Present"
-                Name = "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
-            }
-            WindowsCapability AdminToolsGroupPolicy {
-                Ensure = "Present"
-                Name = "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0"
-            }
-            xPackage AdminToolsSqlServerMgmt {
-                Ensure = "Present"
-                Name = "Microsoft SQL Server Management Studio - 18.6"
-                Path = ("{0}\DSC\Packages\SQLServerMgmtStudio\SSMS-Setup-ENU.exe" -f $wvdConfigDataPath)
-                ProductId = ''
-                Arguments = '/Quiet'
-                IgnoreReboot = $true
-            }
-            PSModuleResource AzurePowerShellModule {
-                Ensure = "Present"
-                Module_Name = "Az"
-            }
-            PSModuleResource AzureADPowerShellModule {
-                Ensure = "Present"
-                Module_Name = "AzureAD"
-            }
-            PSModuleResource WVDPowerShellModule {
-                Ensure = "Present"
-                Module_Name = "Az.DesktopVirtualization"
-            }
-            PSModuleResource SPOPowerShellModule {
-                Ensure = "Present"
-                Module_Name = "Microsoft.Online.SharePoint.PowerShell"
-            }
-            PSModuleResource EXOPowerShellModule {
-                Ensure = "Present"
-                Module_Name = "ExchangeOnlineManagement"
-            }
-            File AdminToolsDesktopFolder {
-                Ensure = "Present"
-                Type = "Directory"
-                DestinationPath = "C:\Users\Public\Desktop\Admin Tools"
-            }
-            Script CopyAdminToolShortcuts {
-                DependsOn = "[File]AdminToolsDesktopFolder"
-                GetScript = { @{ Result = ''} }
-                TestScript = {
-                    If (Test-Path -Path "C:\Users\Public\Desktop\Admin Tools") { Return $false }
-                    Else { Return $true }
-                }
-                SetScript = { Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\*.lnk" -Destination "C:\Users\Public\Desktop\Admin Tools" -Force }
-            }
         }
         xPackage VisualStudioCode {
             Ensure = "Present"
@@ -380,13 +421,13 @@ Configuration WvdSessionHostConfig
             DependsOn = '[xPackage]FsLogix'
         }
         Script RebootPostInstall {
-            TestScript = { return (Test-Path 'C:\ProgramData\WVDConfiguration\RebootPostInstall.txt') }
+            TestScript = { return (Test-Path ("{0}\RebootPostInstall.txt" -f $wvdConfigDataPath)) }
             SetScript = {
-                New-Item -Path "C:\ProgramData\WVDConfiguration\" -Name "RebootPostInstall.txt" -ItemType "file"
+                New-Item -Path $using:wvdConfigDataPath -Name "RebootPostInstall.txt" -ItemType "file"
                 $global:DSCMachineStatus = 1
             }
             GetScript = {
-                @{ Result = (Test-Path 'C:\ProgramData\WVDConfiguration\RebootPostInstall.txt') }
+                @{ Result = (Test-Path ("{0}\RebootPostInstall.txt" -f $wvdConfigDataPath)) }
             }
         }
     }
